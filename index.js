a872b74c7c014c39dfe596aa8800d840f30453fd
Dinesh Reddy <dineshreddy8844@gmail.com>
2025-01-25 12:23:15 +0530

    task4

---
+++ index.js
@@ -0,0 +1,7 @@
+let student = "developer"
+let skills = "html,css,bootstrap,js and react"
+student == "developer" && skills == "html,css,bootstrap" ? console.log("he is a UI/UX Developer") : console.log("he is not a UI/UX developer");
+student == "developer" && skills == "html,css,bootstrap,js and react" ? console.log("he is a Frontend Developer") : console.log("he is not a frontend developer");
+student == "developer" && skills == "html,css,bootstrap,js,react and node.js" ? console.log("he is a Fullstack Developer") : console.log("he is not a fullstack developer");
+student == "developer" && skills == "html,css,bootstrap,js and node.js" ? console.log("he is a Backend Developer") : console.log("he is not a Backend developer");
+student == "developer" && skills == "No skills" ? console.log("join 10kcoders") : console.log("He do have skills");
